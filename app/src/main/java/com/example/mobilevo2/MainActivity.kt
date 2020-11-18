f9006da26e4dbe5f4ad92fec6dc838986d3227be
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2020-11-18 22:30:08 +0100

    add Profile UI, Viewbinding test in MainActivity and Fragment stuff which is not completed yet

---
+++ app/src/main/java/com/example/mobilevo2/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.mobilevo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.mobilevo2.databinding.ActivityMainBinding
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        /*val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main)*/
+        //oldschool mit setConentvie(r.layout.activity.main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //jawoi gschoff mid view binding, voi geil sisd miassds R.iwos mochn
+        //google use viewbindin android erstellt für edes xml eine eigene klasse
+        //developer.android OP
+        //Oldschool: val mynameView: TextView = findViewById(R.id.profileName)
+        //mit viewbindin:
+        binding.profileName.setOnClickListener{
+            binding.profileName.text = "ohaaaaaaaaa viewbinding lul no viewFindById anymore :DD"
+        }
+    }
+}
